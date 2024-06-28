fbf93282fa0a64a34050581e9d72740bfc96cf02
Felix Dobler <felix.dobler@uni-konstanz.de>
2024-06-28 14:49:03 +0200

    switch to big numpy array for csi matrices

--- csi_transform.py
+++ csi_transform.py
@@ -1,5 +1,6 @@
 import struct
 import sys
+import time
 # from npy_append_array import NpyAppendArray
 import numpy as np
 import joblib
@@ -28,7 +29,16 @@ class CSI_Struct:
         self.csi_len = csi_len
         self.buf_len = buf_len
 
-def transformData(inputPath, outputPath = None) -> list[tuple[CSI_Struct, list[list[list[COMPLEX]]]]]:
+def transformData(inputPath, outputPath = None, amplitudeOnly = True):
+    """
+    Transforms the data from the inputPath to a joblib file at outputPath
+    The format of the exported data is a tuple of two lists:
+    - CSI_Status objects
+    - CSI_Matrix array of shape (n, 3, 3, 114, [1/2])
+    """
+
+    startTime = time.time()
+
     if not outputPath:
         outputPath = inputPath + ".joblib"
     
@@ -57,48 +67,48 @@ def transformData(inputPath, outputPath = None) -> list[tuple[CSI_Struct, list[l
         print(f"assumed number of records: {file_size / record_size}")
         presumed_num_records = file_size / record_size
 
+        assert presumed_num_records.is_integer(), "File size is not a multiple of record size"
+        presumed_num_records = int(presumed_num_records)
 
-        # records: list[tuple] = []
+
+        csi_matrices_shape = (presumed_num_records, 3, 3, 114) if amplitudeOnly else (presumed_num_records, 3, 3, 114,2)
+
         csiStatusL = []
-        csiMatrixL = []
-        # with NpyAppendArray(file_path + ".npy") as npaa:
-        while file_size - f.tell() >= record_size:
-                # if file_size - f.tell() >= 100 * record_size:
-                    # for _ in range(100):                    
-                    #     csi = read_csi_struct(f, csi_struct_format, csi_struct_size)
-                    #     csi_matrix = read_csi_matrix(f, complex_format, complex_size)
-                    #     csi_matrix = extract_amplitude(csi_matrix)
-                    #     print(csi_matrix.shape, csi_matrix.dtype)
-                    #     records.append((csi, csi_matrix))
+        csiMatrices = np.empty(csi_matrices_shape, dtype=np.int16)
 
-                    #     npaa.append(np.array(records, dtype=object))
-                    #     records = []
-                # else:
-                    print(f"record n {len(csiStatusL)}, {f.tell() / file_size * 100:.2f}% complete")
-                    csi = read_csi_struct(f, csi_struct_format, csi_struct_size)
+        while file_size - f.tell() >= record_size:
+                    currentRecordNumber = f.tell() // record_size
+
+                    print(f"record n {currentRecordNumber}, {f.tell() / file_size * 100:.2f}% complete")
+                    csi_status = read_csi_struct(f, csi_struct_format, csi_struct_size)
                     csi_matrix = read_csi_matrix(f, complex_format, complex_size)
-                    csi_amp = extract_amplitude(csi_matrix)
-                    # print(csi_amp.shape, csi_amp.dtype)
-                    csiStatusL.append(csi)
-                    csiMatrixL.append(csi_amp)
+
+                    if amplitudeOnly:
+                        csi_matrix = extract_amplitude(csi_matrix)
+                    
+                    # print(.shape, csi_amp.dtype)
+                    csiStatusL.append(csi_status)
+                    csiMatrices[currentRecordNumber] = csi_matrix
                     # records.append((csi, csi_matrix))   
             # npaa.append(np.array(records, dtype=object))
     # records = (csiStatusL, csiMatrixL)
-    joblib.dump((csiStatusL, csiMatrixL), outputPath)
-    return (csiStatusL, csiMatrixL)
+    joblib.dump((csiStatusL, csiMatrices), outputPath)
+    print(csiMatrices.shape, csiMatrices.dtype)
 
+    print(f"{'csi:big np array'} - Transformed {len(csiStatusL)} records in {time.time() - startTime:.2f} seconds")
 
-get_amplitude = np.vectorize(lambda complex: complex.real)
+    return (csiStatusL, csiMatrices)
+
 
 def extract_amplitude(csi_matrix):
-    return get_amplitude(np.array(csi_matrix, dtype=object))
+    return csi_matrix[:, :, :, 0]
 
 def read_csi_struct(f, csi_struct_format, csi_struct_size):
     csi_struct_data = f.read(csi_struct_size)
     csi_struct_values = struct.unpack(csi_struct_format, csi_struct_data)
     return CSI_Struct(*csi_struct_values)
 
-def read_csi_matrix(f, complex_format, complex_size):
+def read_csi_matrix(f, complex_format, complex_size) -> np.ndarray:
     complex_array = []
     for _ in range(3):
         matrix = []
@@ -107,43 +117,15 @@ def read_csi_matrix(f, complex_format, complex_size):
             for _ in range(114):
                 complex_data = f.read(complex_size)
                 real, imag = struct.unpack(complex_format, complex_data)
-                subcarriers.append(COMPLEX(real, imag))
+                subcarriers.append([real, imag])
             matrix.append(subcarriers)
         complex_array.append(matrix)
-    return complex_array
+    return np.array(complex_array, dtype=np.int16)
 
 # use file from first cli argument
 FILE = sys.argv[1]
-records = transformData(FILE)
+
+records = transformData(FILE, f"{FILE}_int16.joblib")
 print(f"Num records: {len(records[0])}")
-# exit()
-# for record in records:
-#     # print(f"{record[0].tstamp} | {record[1][0][0][0].real},{record[1][0][0][0].imag}")
-
-#     print(
-#         record[0].tstamp,
-#         record[0].channel,
-#         record[0].chanBW,
-#         record[0].rate,
-#         record[0].nr,
-#         record[0].nc,
-#         record[0].num_tones,
-#         record[0].noise,
-#         record[0].phyerr,
-#         record[0].rssi,
-#         record[0].rssi_0,
-#         record[0].rssi_1,
-#         record[0].rssi_2,
-#         record[0].payload_len,
-#         record[0].csi_len,
-#         record[0].buf_len, 
-#         sep=",")
-
-#     for i in range(3):
-#         for j in range(3):
-#             for k in range(114):
-#                 print(record[1][i][j][k].real, record[1][i][j][k].imag, sep=",", end="|")
-#     print()        
-
-# np.array(records, dtype=object).dump(FILE + ".npy")
+print(records[1][0][0][0][0], records[1][0].dtype)
 
